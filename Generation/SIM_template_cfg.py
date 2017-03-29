3005351c610a6d1875649699f89e13a4750ea2b9
amassiro <massironi.andrea@gmail.com>
2017-03-29 10:38:39 +0200

    new

--- Generation/SIM_template_cfg.py
+++ Generation/SIM_template_cfg.py
@@ -60,16 +60,16 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
     #)
 )
 
-process.LHEoutput = cms.OutputModule("PoolOutputModule",
-    splitLevel = cms.untracked.int32(0),
-    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
-    outputCommands = process.LHEEventContent.outputCommands,
-    fileName = cms.untracked.string('file:EXO-RunIISummer15wmLHEGS-00000_inLHE.root'),
-    dataset = cms.untracked.PSet(
-        filterName = cms.untracked.string(''),
-        dataTier = cms.untracked.string('LHE')
-    )
-)
+#process.LHEoutput = cms.OutputModule("PoolOutputModule",
+    #splitLevel = cms.untracked.int32(0),
+    #eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    #outputCommands = process.LHEEventContent.outputCommands,
+    #fileName = cms.untracked.string('file:EXO-RunIISummer15wmLHEGS-00000_inLHE.root'),
+    #dataset = cms.untracked.PSet(
+        #filterName = cms.untracked.string(''),
+        #dataTier = cms.untracked.string('LHE')
+    #)
+#)
 
 # Additional output definition
 
@@ -78,51 +78,51 @@ process.LHEoutput = cms.OutputModule("PoolOutputModule",
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_71_V1::All', '')
 
-process.generator = cms.EDFilter("Pythia8HadronizerFilter",
-    pythiaPylistVerbosity = cms.untracked.int32(1),
-    filterEfficiency = cms.untracked.double(1.0),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    comEnergy = cms.double(13000.0),
-    maxEventsToPrint = cms.untracked.int32(1),
-    PythiaParameters = cms.PSet(
-        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
-            'Main:timesAllowErrors = 10000', 
-            'Check:epTolErr = 0.01', 
-            'Beams:setProductionScalesFromLHEF = off', 
-            'SLHA:keepSM = on', 
-            'SLHA:minMassSM = 1000.', 
-            'ParticleDecays:limitTau0 = on', 
-            'ParticleDecays:tau0Max = 10', 
-            'ParticleDecays:allowPhotonRadiation = on'),
-        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
-            'Tune:ee 7', 
-            'MultipartonInteractions:pT0Ref=2.4024', 
-            'MultipartonInteractions:ecmPow=0.25208', 
-            'MultipartonInteractions:expPow=1.6'),
-        processParameters = cms.vstring('SLHA:useDecayTable = off', 
-            '25:m0 = 125.0', 
-            '25:onMode = off', 
-            '25:onIfMatch = 24 -24', 
-            '24:mMin = 0.05', 
-            '24:onMode = off', 
-            '24:onIfAny = 11 13 15 12 14 16 1 2 3 4'),
-        parameterSets = cms.vstring('pythia8CommonSettings', 
-            'pythia8CUEP8M1Settings', 
-            'processParameters')
-    )
-)
+#process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    #pythiaPylistVerbosity = cms.untracked.int32(1),
+    #filterEfficiency = cms.untracked.double(1.0),
+    #pythiaHepMCVerbosity = cms.untracked.bool(False),
+    #comEnergy = cms.double(13000.0),
+    #maxEventsToPrint = cms.untracked.int32(1),
+    #PythiaParameters = cms.PSet(
+        #pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+            #'Main:timesAllowErrors = 10000', 
+            #'Check:epTolErr = 0.01', 
+            #'Beams:setProductionScalesFromLHEF = off', 
+            #'SLHA:keepSM = on', 
+            #'SLHA:minMassSM = 1000.', 
+            #'ParticleDecays:limitTau0 = on', 
+            #'ParticleDecays:tau0Max = 10', 
+            #'ParticleDecays:allowPhotonRadiation = on'),
+        #pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14', 
+            #'Tune:ee 7', 
+            #'MultipartonInteractions:pT0Ref=2.4024', 
+            #'MultipartonInteractions:ecmPow=0.25208', 
+            #'MultipartonInteractions:expPow=1.6'),
+        #processParameters = cms.vstring('SLHA:useDecayTable = off', 
+            #'25:m0 = 125.0', 
+            #'25:onMode = off', 
+            #'25:onIfMatch = 24 -24', 
+            #'24:mMin = 0.05', 
+            #'24:onMode = off', 
+            #'24:onIfAny = 11 13 15 12 14 16 1 2 3 4'),
+        #parameterSets = cms.vstring('pythia8CommonSettings', 
+            #'pythia8CUEP8M1Settings', 
+            #'processParameters')
+    #)
+#)
 
 
-process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    nEvents = cms.untracked.uint32(400),
-    outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
-    numberOfParameters = cms.uint32(1),
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.0/monoHiggs/Zp2HDM/Zprime_A0h_A0chichi/v1/Zprime_A0h_A0chichi_MZp600_MA0300_tarball.tar.xz')
-)
+#process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    #nEvents = cms.untracked.uint32(400),
+    #outputFile = cms.string('cmsgrid_final.lhe'),
+    #scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
+    #numberOfParameters = cms.uint32(1),
+    #args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.0/monoHiggs/Zp2HDM/Zprime_A0h_A0chichi/v1/Zprime_A0h_A0chichi_MZp600_MA0300_tarball.tar.xz')
+#)
 
 
-process.ProductionFilterSequence = cms.Sequence(process.generator)
+#process.ProductionFilterSequence = cms.Sequence(process.generator)
 
 # Path and EndPath definitions
 #process.lhe_step = cms.Path(process.externalLHEProducer)
@@ -131,14 +131,14 @@ process.simulation_step = cms.Path(process.psim)
 #process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
-process.LHEoutput_step = cms.EndPath(process.LHEoutput)
+#process.LHEoutput_step = cms.EndPath(process.LHEoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.simulation_step,process.endjob_step,process.RAWSIMoutput_step,process.LHEoutput_step)
+process.schedule = cms.Schedule(process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
 # filter all path with the production filter sequence
-for path in process.paths:
-        if path in ['lhe_step']: continue
-        getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+#for path in process.paths:
+        #if path in ['lhe_step']: continue
+        #getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
 
 # customisation of the process.
 
